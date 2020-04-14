95505b0892cedc6f4734ee60b633f5ed3afdfe45
jathinbadam <jathinbadam99@gmail.com>
2020-04-14 23:37:00 +0530

    Added gmail login and update profile page

--- Auth/app/src/main/java/com/example/auth/USER.java
+++ Auth/app/src/main/java/com/example/auth/USER.java
@@ -2,7 +2,7 @@ package com.example.auth;
 
 public class USER
 {
-    public String EMAIL,NAME,PHONE,PASSWORD,COMMUNITY;
+    private String EMAIL,NAME,PHONE,PASSWORD,COMMUNITY;
 
     public USER() {
     }
@@ -12,6 +12,46 @@ public class USER
         this.NAME = NAME;
         this.PHONE = PHONE;
         this.PASSWORD = PASSWORD;
+        this.COMMUNITY = COMMUNITY;
+    }
+
+    public String getEMAIL() {
+        return EMAIL;
+    }
+
+    public void setEMAIL(String EMAIL) {
+        this.EMAIL = EMAIL;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public String getPHONE() {
+        return PHONE;
+    }
+
+    public void setPHONE(String PHONE) {
+        this.PHONE = PHONE;
+    }
+
+    public String getPASSWORD() {
+        return PASSWORD;
+    }
+
+    public void setPASSWORD(String PASSWORD) {
+        this.PASSWORD = PASSWORD;
+    }
+
+    public String getCOMMUNITY() {
+        return COMMUNITY;
+    }
+
+    public void setCOMMUNITY(String COMMUNITY) {
         this.COMMUNITY = COMMUNITY;
     }
 }
